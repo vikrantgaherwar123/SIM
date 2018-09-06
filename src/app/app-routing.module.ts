d3f3c6cd1be81736aaf003ad6782768243d14ff6
Satish Logade <satishlogade@gmail.com>
2018-09-06 14:23:26 +0530

    Added font awesome and login page design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
 
+import { AppComponent } from './components/index/index.component'
+import { LoginComponent } from './components/login/login.component';
+
 const routes: Routes = [
-  {}
+  // { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  // { path: 'dashboard', component: AppComponent },
+  { path: 'login', component: LoginComponent }
 ]
 
 @NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
   exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
