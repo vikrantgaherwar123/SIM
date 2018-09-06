edf2f80ad06aac4e30e8d1ebcf8dd33dc6a5f3f0
Satish Logade <satishlogade@gmail.com>
2018-09-06 16:15:46 +0530

    Added dashboard component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { Routes, RouterModule } from '@angular/router'
 
 import { AppComponent } from './components/index/index.component'
 import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component'
 
 const routes: Routes = [
   // { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  // { path: 'dashboard', component: AppComponent },
+  { path: 'dashboard', component: DashboardComponent },
   { path: 'login', component: LoginComponent }
 ]
 
