c9fe204710bd7c32b0a36c4821f207fa03a3279c
Satish Logade <satishlogade@gmail.com>
2018-09-11 11:31:25 +0530

    Style refactored and product page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { Routes, RouterModule } from '@angular/router'
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component'
 import { ClientComponent } from './components/client/client.component'
+import { ProductComponent } from './components/product/product.component'
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'client', component: ClientComponent }
+  { path: 'client', component: ClientComponent },
+  { path: 'product', component: ProductComponent }
 ]
 
 @NgModule({
