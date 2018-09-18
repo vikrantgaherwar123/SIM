0b1715e005104daf34c0b3e0d961c3bacf2d1b80
Satish Logade <satishlogade@gmail.com>
2018-09-18 11:31:03 +0530

    Auth gaurd implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { TranslateModule } from '@ngx-translate/core';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { TranslateModule } from '@ngx-translate/core'
+import { HttpClientModule } from '@angular/common/http'
 import { FormsModule } from '@angular/forms'
-import { CookieService } from 'ngx-cookie-service';
+import { CookieService } from 'ngx-cookie-service'
 
 import { CONSTANTS } from './constants'
 
-import { AppComponent } from './components/index/index.component';
-import { HeaderComponent } from './components/header/header.component';
-import { AppRoutingModule } from './/app-routing.module';
-import { LoginComponent } from './components/login/login.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { ClientComponent } from './components/client/client.component';
-import { ProductComponent } from './components/product/product.component';
-import { InvoiceComponent } from './components/invoice/invoice.component';
+import { AppComponent } from './components/index/index.component'
+import { HeaderComponent } from './components/header/header.component'
+import { AppRoutingModule } from './/app-routing.module'
+import { LoginComponent } from './components/login/login.component'
+import { DashboardComponent } from './components/dashboard/dashboard.component'
+import { SidebarComponent } from './components/sidebar/sidebar.component'
+import { ClientComponent } from './components/client/client.component'
+import { ProductComponent } from './components/product/product.component'
+import { InvoiceComponent } from './components/invoice/invoice.component'
 
 @NgModule({
   declarations: [
