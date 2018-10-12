16ed2c2d94f3e15d932e6daac52c55182ae119f2
Satish Logade <satishlogade@gmail.com>
2018-10-12 16:33:50 +0530

    create product on invoice page, added items as list

--- src/app/reducers/client.reducer.ts
+++ src/app/reducers/client.reducer.ts
@@ -1,9 +1,29 @@
-import { Action } from '@ngrx/store'
 import { client } from '../interface'
 
 import * as ClientActions from '../actions/client.action'
 
-const initialState: client[] = []
+const initialState: client[] = [
+  // Comment this data before publishing
+  // {
+  //   addressLine1: '94/A',
+  //   addressLine2: 'Nandanvan',
+  //   addressLine3: 'Nogpur',
+  //   businessDetail: 'satronics pvt ltd',
+  //   businessId: '',
+  //   contactPersonName: 'satish logade',
+  //   deviceCreatedDate: 1511180754369,
+  //   email: 'satishlogade@gmail.com',
+  //   enabled: 0,
+  //   localClientid: 5,
+  //   modifiedDate: 0,
+  //   name: 'satishL',
+  //   number: '12345678',
+  //   organizationId: 1,
+  //   serverUpdateTime: 1511180755180,
+  //   shippingAddress: 'poonam mall',
+  //   uniqueKeyClient: '@11512392993529bb4e46e8'
+  // }
+]
 
 export function clientReducer(state = initialState, action: ClientActions.Action) {
 	switch (action.type) {
