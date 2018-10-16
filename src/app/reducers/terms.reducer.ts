a11c7f73f729ab9a6b2a16b49461f971f276c83c
Satish Logade <satishlogade@gmail.com>
2018-10-16 18:55:03 +0530

    Add payments functionality done

--- src/app/reducers/terms.reducer.ts
+++ src/app/reducers/terms.reducer.ts
@@ -5,32 +5,32 @@ import * as TermsActions from '../actions/terms.action'
 
 const initialState: terms[] = [
   // Comment this data before publishing
-  {
-    createDate: 0,
-    deviceCreateDate: 1513069212782,
-    enabled: 1,
-    modifiedDate: 0,
-    modifiedOn: 0,
-    orgId: 1,
-    serverTermCondId: 0,
-    serverUpdateTime: 1513069218384,
-    setDefault: 'DEFAULT',
-    terms: "testing terms 1↵testing terms 1",
-    uniqueKeyTerms: "@115039094973269d8f280a"
-  },
-  {
-    createDate: 0,
-    deviceCreateDate: 1513235982330,
-    enabled: 1,
-    modifiedDate: 0,
-    modifiedOn: 0,
-    orgId: 1,
-    serverTermCondId: 0,
-    serverUpdateTime: 1513235984364,
-    setDefault: "",
-    terms: "testing new terms",
-    uniqueKeyTerms: "@11512972930230ab0d012a"
-  }
+  // {
+  //   createDate: 0,
+  //   deviceCreateDate: 1513069212782,
+  //   enabled: 1,
+  //   modifiedDate: 0,
+  //   modifiedOn: 0,
+  //   orgId: 1,
+  //   serverTermCondId: 0,
+  //   serverUpdateTime: 1513069218384,
+  //   setDefault: 'DEFAULT',
+  //   terms: "testing terms 1↵testing terms 1",
+  //   uniqueKeyTerms: "@115039094973269d8f280a"
+  // },
+  // {
+  //   createDate: 0,
+  //   deviceCreateDate: 1513235982330,
+  //   enabled: 1,
+  //   modifiedDate: 0,
+  //   modifiedOn: 0,
+  //   orgId: 1,
+  //   serverTermCondId: 0,
+  //   serverUpdateTime: 1513235984364,
+  //   setDefault: "",
+  //   terms: "testing new terms",
+  //   uniqueKeyTerms: "@11512972930230ab0d012a"
+  // }
 ]
 
 export function termsReducer(state = initialState, action: TermsActions.Action) {
