5e843e74e031de6ec932d8a33d2b056c9c850b0b
nitish <nitish.kumar@tacktile.in>
2019-05-30 14:17:50 +0530

    pattern issue solved

--- src/app/currencyPipe.ts
+++ src/app/currencyPipe.ts
@@ -31,7 +31,7 @@ export class CurrencyPipe implements PipeTransform {
       });
       
       value = formatter.format(value);
-      value = value.replace(/\€/g,"")
+      value = value.slice(0, -1);
       console.log(value);
       
     }else if(this.settings.numberFormat == '##.##.##.###,00'){
@@ -42,7 +42,7 @@ export class CurrencyPipe implements PipeTransform {
       });
       
       value = formatter.format(value);
-      value = value.replace(/\₹/g,"")
+      value = value.substr(1);
       var map = { ',': '.', '.': ',' };
       value = value.replace(/[,.]/g, function (k) {
         return map[k];
@@ -57,7 +57,7 @@ export class CurrencyPipe implements PipeTransform {
       });
       
       value = formatter.format(value);
-      value = value.replace(/\€/g,"")
+      value = value.slice(0, -1);
       
       console.log(value);
       
@@ -78,7 +78,7 @@ export class CurrencyPipe implements PipeTransform {
       });
       
       value = formatter.format(value);
-      value = value.replace(/\₹/g,"")
+      value = value.substr(1);
     }
     return this.myCurrencySymbol +' ' +value
   }
--- src/app/currencyPipeNoSymbol.ts
+++ src/app/currencyPipeNoSymbol.ts
@@ -15,73 +15,68 @@ export class CurrencyPipeNoSymbol implements PipeTransform {
   
   currencyPattern(value){
     if(value){
-    this.settings = JSON.parse(localStorage.getItem('user')).setting
+      this.settings = JSON.parse(localStorage.getItem('user')).setting
+  
 
-    // if (this.settings.currencyText) {
-    //     this.mysymbols = this.CONST.COUNTRIES.filter(symbole => symbole.countryName == this.settings.country)[0].currencyName;
-    // }
-    // else {
-    //     this.mysymbols = this.CONST.COUNTRIES.filter(symbole => symbole.countryName == this.settings.country)[0].currencyCode;
-    // }
-    if(this.settings.numberFormat == '###.###.###,00'){
-
-      var formatter = new Intl.NumberFormat('de-DE', {
-        style: 'currency',
-        currency: 'EUR',
-      });
-      
-      value = formatter.format(value);
-      value = value.replace(/\€/g,"")
-      console.log(value);
-      
-    }else if(this.settings.numberFormat == '##.##.##.###,00'){
-      
-      var formatter = new Intl.NumberFormat('en-IN', {
-        style: 'currency',
-        currency: 'INR',
-      });
-      
-      value = formatter.format(value);
-      value = value.replace(/\₹/g,"")
-      var map = { ',': '.', '.': ',' };
-      value = value.replace(/[,.]/g, function (k) {
-        return map[k];
-      });
-      console.log(value);
+      if(this.settings.numberFormat == '###.###.###,00'){
+  
+        var formatter = new Intl.NumberFormat('de-DE', {
+          style: 'currency',
+          currency: 'EUR',
+        });
+        
+        value = formatter.format(value);
+        value = value.replace(/\€/g,"")
+        console.log(value);
+        
+      }else if(this.settings.numberFormat == '##.##.##.###,00'){
+        
+        var formatter = new Intl.NumberFormat('en-IN', {
+          style: 'currency',
+          currency: 'INR',
+        });
+        
+        value = formatter.format(value);
+        value = value.replace(/\₹/g,"")
+        var map = { ',': '.', '.': ',' };
+        value = value.replace(/[,.]/g, function (k) {
+          return map[k];
+        });
+        console.log(value);
+      }
+      else if(this.settings.numberFormat == '### ### ###,00'){
+        
+        var formatter = new Intl.NumberFormat('Fr', {
+          style: 'currency',
+          currency: 'EUR',
+        });
+        
+        value = formatter.format(value);
+        value = value.replace(/\€/g,"")
+        
+        console.log(value);
+        
+      }else if(this.settings.numberFormat == '###,###,###.00'){
+        
+        var formatter = new Intl.NumberFormat('en-US', {
+          style: 'currency',
+          currency: 'USD',
+        });
+        
+        value = formatter.format(value);
+        value = value.replace(/\$/g,"")
+      }else if(this.settings.numberFormat == '##,##,##,###.00'){
+        
+        var formatter = new Intl.NumberFormat('en-IN', {
+          style: 'currency',
+          currency: 'INR',
+        });
+        
+        value = formatter.format(value);
+        value = value.replace(/\₹/g,"")
+      }
+      return value
     }
-    else if(this.settings.numberFormat == '### ### ###,00'){
-      
-      var formatter = new Intl.NumberFormat('Fr', {
-        style: 'currency',
-        currency: 'EUR',
-      });
-      
-      value = formatter.format(value);
-      value = value.replace(/\€/g,"")
-      
-      console.log(value);
-      
-    }else if(this.settings.numberFormat == '###,###,###.00'){
-      
-      var formatter = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD',
-      });
-      
-      value = formatter.format(value);
-      value = value.replace(/\$/g,"")
-    }else if(this.settings.numberFormat == '##,##,##,###.00'){
-      
-      var formatter = new Intl.NumberFormat('en-IN', {
-        style: 'currency',
-        currency: 'INR',
-      });
-      
-      value = formatter.format(value);
-      value = value.replace(/\₹/g,"")
-    }
-    return value
-  }
 
 }
 
